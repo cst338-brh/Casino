18487f507c6ab2e036320402f9e480749fdd6491
hmccabe <hmccabe@csumb.edu>
2016-09-13 16:09:39 -0700

    moved braces, updated saveWinnings return

--- Casino/src/Assig2.java
+++ Casino/src/Assig2.java
@@ -9,29 +9,24 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Assig2 
-{
-   public static Scanner input = new Scanner(System.in);
+public class Assig2 {
+   private static Scanner input = new Scanner(System.in);
 
-   public static void main(String[] args) 
-   {
-      
+   public static void main(String[] args) {
       int betMade = getBet();
-      int pullCounter = 0;
       TripleString casinoSpin = new TripleString();
       
-      while (betMade != 0 && pullCounter < 40) 
-      {
+      while (betMade != 0) {
         TripleString currentSpin = pull();
         int payoutMultiplier = getPayMultiplier(currentSpin);
         int payout = payoutMultiplier * betMade;
         display(currentSpin, payout);
-        casinoSpin.saveWinnings(payout);
+        currentSpin.saveWinnings(payout);
         
-        
-        pullCounter ++; 
         betMade = getBet();
       }
+      
+      input.close();
            
       // Testing for getBet
       /*
@@ -68,7 +63,6 @@ public class Assig2
       //TripleString ts = new TripleString();         
       //ts.test(); // TODO: Remove this!
       
-      input.close();
    }
    
    // Prompt the user for an integer between 0 and 100 and return result
@@ -84,8 +78,7 @@ public class Assig2
    }
    
    // Return a TripleString object which contains 3 randomized slot machine result strings
-   public static TripleString pull() 
-   {
+   public static TripleString pull() {
       TripleString returnTuple = new TripleString();
       returnTuple.setString1(randString());
       returnTuple.setString2(randString());      
@@ -95,8 +88,7 @@ public class Assig2
    }
    
    // Return a string randomly selected from four possible slot machine results
-   public static String randString() 
-   {
+   public static String randString() {
       String returnString = "ERROR";
       double internalRandom = Math.random();
       
@@ -115,39 +107,26 @@ public class Assig2
    }
    
    // Return the appropriate winnings multiplier based on the slot machine result strings
-   public static int getPayMultiplier(TripleString thePull) 
-   {
+   public static int getPayMultiplier(TripleString thePull) {
       int payoutMult = 0;
       
-      if (thePull.getString1() == "cherries")
-      {
-         if (thePull.getString2() == "cherries")
-         {
-            if (thePull.getString3() == "cherries")
-            {
+      if (thePull.getString1() == "cherries") {
+         if (thePull.getString2() == "cherries") {
+            if (thePull.getString3() == "cherries") {
                payoutMult = 30;
-            }
-            else
-            {
+            } else {
                payoutMult = 15;
             }
-         }
-         else
-         {
+         } else {
             payoutMult = 5;
          }
-      }
-      else if (thePull.getString1() == "BAR" && thePull.getString2() == "BAR" && thePull.getString3() == "BAR")
-      {
+      } else if (thePull.getString1() == "BAR" && thePull.getString2() == "BAR" && thePull.getString3() == "BAR") {
          payoutMult = 50;
-      }
-      else if (thePull.getString1() == "7" && thePull.getString2() == "7" && thePull.getString3() == "7")
-      {
+      } else if (thePull.getString1() == "7" && thePull.getString2() == "7" && thePull.getString3() == "7") {
          payoutMult = 100;
       }
       
-      return payoutMult;
-      
+      return payoutMult;     
    }
    
    // Display the results of a pull and associated winnings
@@ -162,10 +141,10 @@ public class Assig2
       } else {
          System.out.print("Sorry - you lost.");
       }
+   }
 }
 
-class TripleString 
-{
+class TripleString {
    public static final int MAX_LEN = 20;     // Max allowable length for Strings
    public static final int MAX_PULLS = 40;   // Max number of pulls
    private static int numPulls = 0;          // Pull count
@@ -173,8 +152,7 @@ class TripleString
    private String string1,string2,string3;
    
    // Default constructor
-   public TripleString() 
-   {
+   public TripleString() {
       string1 = "";
       string2 = "";
       string3 = "";
@@ -182,109 +160,84 @@ class TripleString
    
    // Check that a given string is not null and is less than or equal to the maximum allowable length
    // Return true if string is valid; otherwise return false
-   private boolean validString(String str) 
-   {
-      if (str != null && str.length() <= MAX_LEN) 
-      {
+   private boolean validString(String str) {
+      if (str != null && str.length() <= MAX_LEN) {
          return true;
-      } 
-      else 
-      {
+      } else {
          return false;
       }
    }
 
    // Return the value of string1
-   public String getString1() 
-   {
+   public String getString1() {
       return string1;
    }
    
    // Return the value of string2
-   public String getString2() 
-   {
+   public String getString2() {
       return string2;
    }
    
    // Return the value of string3
-   public String getString3() 
-   {
+   public String getString3() {
       return string3;
    }
    
    // Update the value of string1 to the given string, if valid
    // Return true if update is successful; otherwise return false
-   public boolean setString1(String str) 
-   {
-      if (validString(str)) 
-      {
+   public boolean setString1(String str) {
+      if (validString(str)) {
          string1 = str;
          return true;
-      } 
-      else 
-      {
+      } else {
          return false;
       }
    }
    
    // Update the value of string2 to the given string, if valid
    // Return true if update is successful; otherwise return false
-   public boolean setString2(String str) 
-   {
-      if (validString(str)) 
-      {
+   public boolean setString2(String str) {
+      if (validString(str)) {
          string2 = str;
          return true;
-      } 
-      else 
-      {
+      } else {
          return false;
       }
    }
    
    // Update the value of string3 to the given string, if valid
    // Return true if update is successful; otherwise return false
-   public boolean setString3(String str) 
-   {
-      if (validString(str)) 
-      {
+   public boolean setString3(String str) {
+      if (validString(str)) {
          string3 = str;
          return true;
-      } 
-      else 
-      {
+      } else {
          return false;
       }
    }
    
    // Return a string containing the values of string1, string2, and string3
-   public String toString() 
-   {
+   public String toString() {
       return string1 + " " + string2 + " " + string3;
    }
    
-   // If MAX_PULLS count has not been reached, add winnings value to array and update pull count
-   // Return true if insertion is successful; otherwise return false
-   public boolean saveWinnings(int winnings) 
-   {
-      if (numPulls < MAX_PULLS) 
-      {
-         pullWinnings[numPulls] = winnings;
-         numPulls++;
+   // Add winnings value to array and update pull count
+   // Return true if pull count is less than MAX_PULLS; otherwise return false
+   public boolean saveWinnings(int winnings) {
+      pullWinnings[numPulls] = winnings;
+      numPulls++;
+      
+      if (numPulls < MAX_PULLS) {
          return true;
-      } 
-      else 
-      {
+      } else {
          return false;
       }
    }
    
    // Return string containing all winnings values
-   public String displayWinnings() 
-   {
+   public String displayWinnings() {
       String winnings = "";
-      for (int i = 0; i < numPulls; i++) 
-      {
+      for (int i = 0; i < numPulls; i++) {
          winnings += " " + pullWinnings[i];
       }
       return winnings;
